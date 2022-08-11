fbf87f27271f3e51404d4e95998d0c6b24322340
Natalia Monasturska <tjomamon@gmail.com>
2022-08-11 18:41:27 +0300

    end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
@@ -6,92 +6,74 @@ import { ContactList } from 'components/ContactList/ContactList';
 import css from './App.module.css';
 const LS_KEY = 'reader_contacts';
 
-export class App extends Component {
-  state = {
-    contacts: [
+export const App = () => {
+  const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem(LS_KEY,))
+    ?? [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount(){
-    const contacts = JSON.parse(localStorage.getItem(LS_KEY,));
-    console.log(typeof contacts) ;
-    if(contacts){
-      this.setState({contacts})
-    }
-  
-  }
-  componentDidUpdate(_, prevState){
-   if(prevState.contacts !== this.state.contacts){
-    localStorage.setItem(LS_KEY,JSON.stringify(this.state.contacts))
-   }
-  }
+    ]
 
-  handleChangeInputFilter = event => {
-    this.setState({
-      filter: event.target.value,
-    });
+  );
+  const [filterName, setFilterName] = useState('')
+
+
+  const FilterNormalize = filterName.toLowerCase();
+
+  const filterContacts = contacts.filter(({ name }) => {
+    return name.toLowerCase().includes(FilterNormalize)
+  })
+
+  useEffect(() => {
+    window.localStorage.setItem(LS_KEY, JSON.stringify(contacts))
+  }, [contacts])
+
+
+  const handleChangeInputFilter = event => {
+    setFilterName(event.target.value)
   };
 
-  getFilterContactsByName = () => {
-    const { filter, contacts } = this.state;
-    const FilterNormalize = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(FilterNormalize)
-    );
-  };
-
-  recordsСontactsInState = (name, number) => {
-   const nameNormalized = name.toLowerCase();
-    const dobbleName = this.state.contacts.find(
+  const recordsСontactsInState = (name, number) => {
+    const nameNormalized = name.toLowerCase();
+    const dobbleName = contacts.find(
       contact => contact.name.toLowerCase() === nameNormalized
     );
     dobbleName
       ? window.alert(`${name} is already in contacts`)
-      : this.setState(prevState => {
-          return {
-            contacts: [
-              ...prevState.contacts,
-              {
-                id: nanoid(),
-                name,
-                number,
-              },
-            ],
-          };
-        });
+      :
+      setContacts(prevContacts => [
+        ...prevContacts,
+        {
+          id: nanoid(),
+          name,
+          number,
+        },
+      ])
   };
 
-  handleDeleteClick = id => {
-    const newContacts = this.state.contacts.filter(
+  const handleDeleteClick = id => {
+    const newContacts = contacts.filter(
       contact => contact.id !== id
     );
-    this.setState({
-      contacts: [...newContacts],
-    });
+    setContacts([...newContacts])
   };
 
-  render() {
-    const filtercontactsByName = this.getFilterContactsByName();
+  return (
+    <div className={css.appContainer}>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm recordsСontactsInState={recordsСontactsInState} />
 
-    return (
-      <div className={css.appContainer}>
-        <h1 className={css.title}>Phonebook</h1>
-        <ContactForm recordsСontactsInState={this.recordsСontactsInState} />
-
-        <h2 className={css.title}>Contacts</h2>
-        <Filter
-          filterContact={this.state.filter}
-          onChangeFilter={this.handleChangeInputFilter}
-        />
-        <ContactList
-          contacts={filtercontactsByName}
-          onDeleteClick={this.handleDeleteClick}
-        />
-      </div>
-    );
-  }
+      <h2 className={css.title}>Contacts</h2>
+      <Filter
+        filterContact={filterName}
+        onChangeFilter={handleChangeInputFilter}
+      />
+      <ContactList
+        contacts={filterContacts}
+        onDeleteClick={handleDeleteClick}
+      />
+    </div>
+  );
 }
+
